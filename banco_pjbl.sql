03a9663912d19b72508e13e27abb943e50cfcd23
GeandroMarques <166722959+GeandroMarques@users.noreply.github.com>
2026-04-10 14:00:03 -0300

    Add files via upload

--- banco_pjbl.sql
+++ banco_pjbl.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `banco_pjbl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `banco_pjbl`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: banco_pjbl
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-10 13:42:35
+-- Dump completed on 2026-04-10 13:58:32
